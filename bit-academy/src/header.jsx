8855aa3b57e696453bb59263c63863cb05c0e663
Chukuameke <pm9966178@gmail.com>
2024-07-12 12:48:52 +0100

    updated

--- bit-academy/src/header.jsx
+++ bit-academy/src/header.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
-
+import Logo from './img/bit-logo.png'
 function Header() {
-  return (
-    <div>
-     <div className="navBar">
-        <div className="navbar-logo">
+    return (
+        <>
+            <nav>
+                <div className="navBar">
+                    <div className="navbar-logo">
+                        <img src={Logo} alt="" />
+                    </div>
+                    <div className="nav-bar-list" id='navbar'>
+                     <div className="nav-list">
+                     <ul>
+                            <a href="index.html">
+                                <li>Home</li>
+                            </a>
 
-        </div>
-        <div className="nav-bar-list">
-            
-        </div>
-        </div> 
-    </div>
-  )
+                            <a href="index.html">
+                                <li>Program</li>
+                            </a>
+
+                            <a href="index.html">
+                                <li>Contact</li>
+                            </a>
+                        </ul>
+                     </div>
+                     <div className="navbar-btn">
+                        <button>Get Started</button>
+                     </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Header
