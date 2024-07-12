cf146a6a7d3ba57a6814b99e411cad3853e5579a
Chukuameke <pm9966178@gmail.com>
2024-07-12 13:58:21 +0100

    added navbar

--- bit-academy/src/header.jsx
+++ bit-academy/src/header.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import Logo from './img/bit-logo.png'
-function Header() {
+import React, { Component } from 'react';
+import Logo from './img/bit-logo.png';
+
+class Header extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      clicked: !prevState.clicked
+    }));
+  };
+
+  render() {
     return (
-        <>
-            <nav>
-                <div className="navBar">
-                    <div className="navbar-logo">
-                        <img src={Logo} alt="" />
-                    </div>
-                    <div className="nav-bar-list" id='navbar'>
-                     <div className="nav-list">
-                     <ul>
-                            <a href="index.html">
-                                <li>Home</li>
-                            </a>
-
-                            <a href="index.html">
-                                <li>Program</li>
-                            </a>
-
-                            <a href="index.html">
-                                <li>Contact</li>
-                            </a>
-                        </ul>
-                     </div>
-                     <div className="navbar-btn">
-                        <button>Get Started</button>
-                     </div>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
+      <>
+        <nav>
+          <div className="navBar">
+            <div className="navbar-logo">
+              <img src={Logo} alt="Logo" />
+            </div>
+            <div className="nav-bar-list" id='navbar'>
+              <div className="nav-list">
+                <ul>
+                  <a href="index.html">
+                    <li>Home</li>
+                  </a>
+                  <a href="index.html">
+                    <li>Program</li>
+                  </a>
+                  <a href="index.html">
+                    <li>Contact</li>
+                  </a>
+                </ul>
+              </div>
+              <div className="navbar-btn">
+                <button>Get Started</button>
+              </div>
+            </div>
+          </div>
+          <div id='mobile' onClick={this.handleClick}>
+            <i id='bar' className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </nav>
+      </>
+    );
+  }
 }
 
-export default Header
+export default Header;
+
