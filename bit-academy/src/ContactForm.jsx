cf6ecd41ae2ce9beef4289732198e0f415b44aca
Chukuameke <pm9966178@gmail.com>
2024-07-19 22:51:35 +0100

    ll

--- bit-academy/src/ContactForm.jsx
+++ bit-academy/src/ContactForm.jsx
@@ -111,71 +111,73 @@ const Form = () => {
                 />
               </div>
               <div className='skills-option'>
-              <div className="form-text">
-                <p>FILL IN PERSONAL DETAILS</p>
-              </div>
-              < div className="flex-skill-container">
-              <div className="skill-container">
-                <div>
-                  <input
-                    type="checkbox"
-                    id="skill-graphic-design"
-                    name="skill"
-                    value="Graphic design"
-                    onChange={handleChange}
-                    checked={formData.skills.includes("Graphic design")}
-                  />
-                  <label htmlFor="skill-graphic-design">Graphic design</label>
-                  <input
-                    type="checkbox"
-                    id="skill-ui-ux-design"
-                    name="skill"
-                    value="UI/UX design"
-                    onChange={handleChange}
-                    checked={formData.skills.includes("UI/UX design")}
-                  />
-                  <label htmlFor="skill-ui-ux-design">UI/UX design</label>
+                <div className="form-text">
+                  <p>SKILL OF INTEREST</p>
                 </div>
+                < div className="flex-skill-container">
+                  <div className="skill-container">
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="skill-graphic-design"
+                        name="skill"
+                        value="Graphic design"
+                        onChange={handleChange}
+                        checked={formData.skills.includes("Graphic design")}
+                      />
+                      <label htmlFor="skill-graphic-design">Graphic design</label>
+                      <input
+                        className='skill2'
+                        type="checkbox"
+                        id="skill-ui-ux-design"
+                        name="skill"
+                        value="UI/UX design"
+                        onChange={handleChange}
+                        checked={formData.skills.includes("UI/UX design")}
+                      />
+                      <label htmlFor="skill-ui-ux-design">UI/UX design</label>
+                    </div>
 
-                <div>
-                  <input
-                    type="checkbox"
-                    id="skill-digital-marketing"
-                    name="skill"
-                    value="Digital Marketing"
-                    onChange={handleChange}
-                    checked={formData.skills.includes("Digital Marketing")}
-                  />
-                  <label htmlFor="skill-digital-marketing">Digital Marketing</label>
-                  <input
-                    type="checkbox"
-                    id="skill-data-analysis"
-                    name="skill"
-                    value="Data Analysis"
-                    onChange={handleChange}
-                    checked={formData.skills.includes("Data Analysis")}
-                  />
-                  <label htmlFor="skill-data-analysis">Data Analysis</label>
-                </div>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="skill-digital-marketing"
+                        name="skill"
+                        value="Digital Marketing"
+                        onChange={handleChange}
+                        checked={formData.skills.includes("Digital Marketing")}
+                      />
+                      <label htmlFor="skill-digital-marketing">Digital Marketing</label>
+                      <input
+                        className='skill1'
+                        type="checkbox"
+                        id="skill-data-analysis"
+                        name="skill"
+                        value="Data Analysis"
+                        onChange={handleChange}
+                        checked={formData.skills.includes("Data Analysis")}
+                      />
+                      <label htmlFor="skill-data-analysis">Data Analysis</label>
+                    </div>
 
-                <div>
-                  <input
-                    type="checkbox"
-                    id="skill-ai-integration"
-                    name="skill"
-                    value="AI Integration"
-                    onChange={handleChange}
-                    checked={formData.skills.includes("AI Integration")}
-                  />
-                  <label htmlFor="skill-ai-integration">AI Integration</label>
+                    <div>
+                      <input
+                        type="checkbox"
+                        id="skill-ai-integration"
+                        name="skill"
+                        value="AI Integration"
+                        onChange={handleChange}
+                        checked={formData.skills.includes("AI Integration")}
+                      />
+                      <label htmlFor="skill-ai-integration">AI Integration</label>
+                    </div>
+                  </div>
+                  <div className="invisble">
+
+                  </div>
                 </div>
               </div>
-              <div className="invisble">
-              
-              </div>
-              </div>
-              </div>
-              <button type="submit">Send</button>
+              <button type="submit" className='form-btn'>Send</button>
             </form>
           </div>
         </div>
